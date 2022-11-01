06c144fb4a98d5a5fc53c98e1afa77d3e007eab1
taylor <taylor.viars@yahoo.com>
2022-11-01 16:11:54 -0400

    commit tree efead60bd14c401aa1ef9e9ceb94a87d38c306b6

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+console.log(multiply);
 
-// Write your code here
+let random = 6;
+Math.floor(Math.random() * 10) + 1;
+
+let num3 = 19;
+let num4 = 5;
+let mod = num3 % num4;
+console.log(mod);
+
+let max = 20;
+Math.max(1, 5, 10, 15, 20);
